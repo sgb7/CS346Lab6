8b2d898b14ed3fdfa48846663e7c8ac8262c8053
sgb7 <sgbanister@gmail.com>
2020-10-02 13:19:00 -0700

    Uploaded files

---
+++ src/ExampleGame/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Player LocalPlayer { get; set; }
+        Player RemotePlayer { get; set; }
+        string ClientIpAddress { get; set; }
+        Thread serverThread = null;
+        int ListenPort = 3461;
+        int RemotePort = 3461;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClientIpAddress = "";
+            LocalPlayer = new Player();
+            RemotePlayer = new Player();
+            RemotePlayer.PlayerShape.Fill = Brushes.BlueViolet;
+
+            this.MouseMove += GameScene_MouseMove;
+
+            InitializePlayer(LocalPlayer);
+            InitializePlayer(RemotePlayer);
+
+        }
+
+        private void GameScene_MouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(GameScene);
+            position.X -= LocalPlayer.ActualWidth / 2;
+            position.Y -= LocalPlayer.ActualHeight / 2;
+            if(position.X > 0 && position.Y > 0)
+            {
+                Canvas.SetTop(LocalPlayer, position.Y);
+                Canvas.SetLeft(LocalPlayer, position.X);
+            }
+            SendUpdate(position.X, position.Y);
+        }
+
+        void InitializePlayer(Player p)
+        {
+            GameScene.Children.Add(p);
+            Canvas.SetLeft(p, 0);
+            Canvas.SetTop(p, 0);
+        }
+
+
+        private void SetRemoteIpClicked(object sender, RoutedEventArgs e)
+        {
+            Settings s = new Settings();
+            s.ShowDialog();
+            if(s.ModalResult == true)
+            {
+                ClientIpAddress = s.IpAddress;
+                ListenPort = s.LocalPort;
+                RemotePort = s.RemotePort;
+
+                //I think this will create a new thread every time we edit settings.
+                //Definitely not a great idea but works for our proof of concept.
+                StartServerLoop();
+            }
+        }
+
+        private void SendUpdate(double xPos, double yPos)
+        {
+            UdpClient client = new UdpClient();
+            byte[] package = new byte[1024];
+
+            //TODO: build package payload
+            try
+            {
+                if(ClientIpAddress.Length > 0)
+                {
+                    client.Send(package, package.Length, ClientIpAddress, RemotePort);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
+        }
+
+        private void StartServerLoop()
+        {
+            ThreadStart ts = () => { ServerLoop(); };
+            serverThread = new Thread(ts);
+            serverThread.Start();
+        }
+
+        private void ServerLoop()
+        {
+            UdpClient client = null;
+            try
+            {
+                client = new UdpClient(ListenPort);
+                IPEndPoint remoteClient = new IPEndPoint(IPAddress.Any, 0);
+                while(true)
+                {
+                    byte[] clientData = client.Receive(ref remoteClient);
+                    Debug.Write("data received");
+                    //TODO: process data
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.Write(ex.Message);
+
+                //restart server thread on error
+                Thread.Sleep(500);
+                StartServerLoop();
+            }
+        }
+    }
+}
---
+++ src/ExampleGame/Settings.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExampleGame
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public string IpAddress
+        {
+            get;
+            private set;
+        }
+
+        public int RemotePort { get; set; }
+        public int LocalPort { get; set; }
+        public bool ModalResult { get; private set; }
+        
+
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            IpAddress = ClientIpAddress.Text;
+            int port = -1;
+            if(Int32.TryParse(ClientRemotePort.Text, out port))
+            {
+                RemotePort = port;
+            }
+            if(Int32.TryParse(ClientRemotePort.Text, out port))
+            {
+                LocalPort = port;
+            }
+            ModalResult = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            ModalResult = false;
+            this.Close();
+        }
+    }
+}
